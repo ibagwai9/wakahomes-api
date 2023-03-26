8e66c78a9727d5986d8781f183985267a94569e4
ibagwai9 <ibagwai9@gmail.com>
2023-03-26 12:17:35 +0100

    changes

--- src/models/index.js
+++ src/models/index.js
@@ -5,35 +5,19 @@ const path =  require("path");
 const  Sequelize  = require("sequelize");
 require("dotenv").config();
 
-const env = process.env.NODE_ENV || 'development';
 const basename = path.basename(__filename);
-const config = require(__dirname + '/../config/pgdb.js')[env];
 
 const db = {};
-const use_query_string = true;
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-  } else if (use_query_string) {
-    sequelize = new Sequelize(process.env.DB_CONNECTION_URL, {
-      dialect: "postgres",
-      dialectOptions: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    });
-} else {
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,{
-        url:config.url,
-        host:config.host,
-        dialect:config.dialect,
-        ssl:config.ssl
-    }
-  );
-}
+
+sequelize = new Sequelize(process.env.DB_CONNECTION_URL, {
+  dialect: "postgres",
+  dialectOptions: {
+    require: true,
+    rejectUnauthorized: false,
+  },
+});
+
 fs.readdirSync(__dirname)
   .filter((file) => {
     return (
